535ad639b82c5b4b6505d9862d42f520845e37b6
steven-halla <steven.r.halla@gmail.com>
2024-05-06 10:58:03 -0700

    first

---
+++ home.php
@@ -0,0 +1,20 @@
+<?php
+  require('header.php');
+?>
+
+<?php
+
+echo "<p style='color: red; font-weight: bold; font-size: 18px;'>Steven Halla, CIS 243</p>";
+?>
+
+  <!-- page content -->
+  <section>
+     <h2>Welcome to the home of TLA Consulting.</h2>
+     <p>Please take some time to get to know us.</p>
+     <p>We specialize in serving your business needs
+     and hope to hear from you soon.</p>
+  </section>
+<?php
+  require('footer.php');
+?>
+
